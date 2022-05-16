79ba024aabec07f80c5fff8bf02f829a2628906b
muddassar-lab <muddassarsaleem89@gmail.com>
2022-05-16 19:11:05 +0500

    changed the sign function.

--- backend/helpers/stringHelpers.js
+++ backend/helpers/stringHelpers.js
@@ -1,9 +1,15 @@
 const signChecker = (query) => {
-  var sign;
-  if (query[0] === "-") {
-    sign = -1;
-  } else {
-    sign = 1;
+  var sign = 1;
+  var numberAppeared = false;
+  for (const character of query) {
+    if (character >= "0" && character <= "9") {
+      numberAppeared = true;
+      break;
+    }
+    if (character == "-" && numberAppeared == false) {
+      sign = -1;
+      continue;
+    }
   }
   return sign;
 };
